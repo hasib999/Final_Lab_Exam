4da937063705f8992e1bf317b0fe449774a4d760
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-09-08 13:57:28 +0600

    Create portal.sql

---
+++ DB Schema/portal.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2020 at 09:56 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(11) NOT NULL,
+  `password` varchar(11) NOT NULL,
+  `type` varchar(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `cname` varchar(50) NOT NULL,
+  `mobile` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`, `type`, `name`, `cname`, `mobile`) VALUES
+('admin', 'admin', 'admin', '', '', ''),
+('admin', 'admin', 'admin', '', '', ''),
+('employee', '1234', 'employee', '', '', ''),
+('', '', '', '', '', ''),
+('employee', '1234', 'employee', '', '', ''),
+('', '', '', '', '', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
